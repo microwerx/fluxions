67dc5ef3567c05f0b392b1f2758838570057e6d9
Jonathan Metzgar <microwerx@outlook.com>
2019-11-17 21:16:25 -0900

    add Vf::RootWindow

---
+++ include/viperfish_root_window.hpp
@@ -0,0 +1,15 @@
+#ifndef VIPERFISH_ROOT_WINDOW_HPP
+#define VIPERFISH_ROOT_WINDOW_HPP
+
+#include <viperfish_widget.hpp>
+
+namespace Vf
+{
+	class RootWindow : public Widget {
+	public:
+		RootWindow(const std::string& name);
+		virtual ~RootWindow() override;
+	};
+}
+
+#endif
---
+++ src/viperfish_root_window.cpp
@@ -0,0 +1,13 @@
+#include <viperfish_root_window.hpp>
+
+namespace Vf
+{
+	RootWindow::RootWindow(const std::string& name)
+		: Widget(name) {
+
+	}
+
+	RootWindow::~RootWindow() {
+
+	}
+}
