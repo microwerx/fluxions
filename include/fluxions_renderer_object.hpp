496f2aafc5cba12d6d6c86ebce4dd568ba6f0c3c
Jonathan Metzgar <microwerx@outlook.com>
2019-12-20 13:10:16 -0900

    change name() return from string to const char*

--- include/fluxions_renderer_object.hpp
+++ include/fluxions_renderer_object.hpp
@@ -10,16 +10,26 @@ namespace Fluxions {
 		RendererObject();
 		virtual ~RendererObject();
 
+		// RendererObject::init() should be called before other initialization code
 		virtual void init(const std::string& newname, RendererObject* pparent = nullptr);
+
+		// RendererObject::kill() should be called after other deinitialization code
 		virtual void kill();
+
+		// RendererObject::reset() should only be called if this object has been initialized
 		virtual void reset();
+
+		// type() returns an RTTI string of the type of object
 		virtual const char* type() const;
 
-		const std::string& name() const noexcept { return name_; };
+		const char* name() const noexcept { return name_.c_str(); };
+		const bool initialized() const noexcept { return initialized_; }
 
 		void setParent(RendererObject* pparent);
 		RendererObject* parent();
+		const RendererObject* parent() const;
 	private:
+		bool initialized_ = false;
 		std::string name_;
 		RendererObject* parent_{ nullptr };
 	};
