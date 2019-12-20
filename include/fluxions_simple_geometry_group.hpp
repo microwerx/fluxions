d2d839303af6d0cbfc125b2d62e49b266cefaa51
Jonathan Metzgar <microwerx@outlook.com>
2019-12-20 13:11:24 -0900

    add inheritance from new SimpleSceneGraphNode class

--- include/fluxions_simple_geometry_group.hpp
+++ include/fluxions_simple_geometry_group.hpp
@@ -2,26 +2,23 @@
 #define FLUXIONS_SIMPLE_GEOMETRY_GROUP_HPP
 
 #include <string>
-#include <fluxions_gte_matrix4.hpp>
 #include <fluxions_gte_bounding_box.hpp>
-#include <fluxions_opengl.hpp>
 #include <fluxions_file_path_info.hpp>
+#include <fluxions_simple_scene_graph_node.hpp>
 
 namespace Fluxions
 {
-	struct SimpleGeometryGroup
+	struct SimpleGeometryGroup : SimpleSceneGraphNode
 	{
-		Matrix4f transform;
-		Matrix4f addlTransform;
-		BoundingBoxf bbox;
-
 		FilePathInfo fpi;
 
 		std::string objectName;
 		std::string mtllibName;
 
-		GLuint objectId = 0;
-		GLuint mtllibId = 0;
+		unsigned objectId = 0;
+		unsigned mtllibId = 0;
+
+		virtual const char* nodetype() const noexcept { return "SimpleGeometryGroup"; }
 	};
 } // namespace Fluxions
 
