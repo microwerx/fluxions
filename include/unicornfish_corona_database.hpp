77bc7ee0479190aaad3fd82a08079c83efd2c647
Jonathan Metzgar <microwerx@outlook.com>
2019-12-20 13:12:31 -0900

    decouple renderer from scene graph components

--- include/unicornfish_corona_database.hpp
+++ include/unicornfish_corona_database.hpp
@@ -1,49 +1,24 @@
-// SSPHH/Fluxions/Unicornfish/Viperfish/Hatchetfish/Sunfish/Damselfish/GLUT Extensions
-// Copyright (C) 2017 Jonathan Metzgar
-// All rights reserved.
-//
-// This program is free software : you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.If not, see <https://www.gnu.org/licenses/>.
-//
-// For any other type of licensing, please contact me at jmetzgar@outlook.com
 #ifndef FLUXIONS_CORONA_DATABASE_HPP
 #define FLUXIONS_CORONA_DATABASE_HPP
 
-#include <string>
-#include <vector>
-#include <map>
-#include <algorithm>
+#include <fluxions_stdcxx.hpp>
 #include <fluxions_gte.hpp>
-#include <fluxions_simple_renderer.hpp>
-#include <fluxions_bbox.hpp>
-#include <fluxions_precompiled_models.hpp>
 
 namespace Uf
 {
 	class CoronaDatabase
 	{
-	private:
 	public:
-		Fluxions::SimpleRenderer_GLuint renderer;
+		//Fluxions::SimpleRenderer_GLuint renderer;
 		std::map<std::string, Fluxions::OBJStaticModel> models;
 
 		CoronaDatabase();
 		~CoronaDatabase();
 
 		void LoadOBJ(const std::string& objname, const std::string& filename);
-		void BuildBuffers();
-		void Render();
-		void RenderZOnly();
+		//void BuildBuffers();
+		//void Render();
+		//void RenderZOnly();
 	};
 } // namespace Fluxions
 
