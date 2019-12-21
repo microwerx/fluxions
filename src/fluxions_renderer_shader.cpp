027b63a0f555fc03449485dab52b1d33b16cdd83
Jonathan Metzgar <microwerx@outlook.com>
2019-12-20 16:08:39 -0900

    remove construction of smart pointer from CompileShaderFromFile

--- src/fluxions_renderer_shader.cpp
+++ src/fluxions_renderer_shader.cpp
@@ -2,7 +2,7 @@
 #include <fluxions_renderer_shader.hpp>
 
 namespace Fluxions {
-	RendererShaderPtr CompileShaderFromFile(RendererShaderPtr& shader, GLenum type, const std::string& filename) {
+	bool CompileShaderFromFile(RendererShaderPtr& shader, GLenum type, const std::string& filename) {
 		const char* typeName =
 			(type == GL_VERTEX_SHADER) ? "vertex" :
 			(type == GL_FRAGMENT_SHADER) ? "fragment" :
@@ -12,39 +12,43 @@ namespace Fluxions {
 		FilePathInfo fpi(filename);
 		if (!fpi.IsFile()) {
 			HFLOGERROR("file '%s' does not exist", filename.c_str());
-			return shader;
+			return false;
 		}
 		HFLOGDEBUG("loading %s shader `%s'", typeName, fpi.fullfname.c_str());
 
 		if (shader->shader == 0) {
 			HFLOGERROR("%s shader could not be created!", typeName);
-			return shader;
+			return false;
 		}
 		shader->compileSource(ReadTextFile(filename));
 		
 		if (shader->hadError) {
 			HFLOGERROR("shader %d compile error for %s\n%s", shader->shader, fpi.fullfname.c_str(), shader->infoLog.c_str());
+			return false;
 		}
 		if (shader->didCompile) {
 			HFLOGDEBUG("shader %d compiled", shader->shader);
+			return true;
 		}
 
-		return shader;
+		return false;
 	}
 
 	RendererShader::RendererShader() {}
 
 	RendererShader::~RendererShader() {}
 
-	void RendererShader::init(const std::string& name, RendererObject* pparent, GLenum shaderType) {
+	void RendererShader::init(const std::string& name, RendererObject* pparent, GLenum shaderType_) {
 		RendererObject::init(name, pparent);
-		create_shader(shaderType);
+		create_shader(shaderType_);
 	}
 
 	void RendererShader::kill() {
 		RendererObject::kill();
 		delete_shader();
 	}
+
+	const char* RendererShader::type() const noexcept { return "RendererShader"; }
 
 	bool RendererShader::compileSource(const std::string& sourceCode) {
 		source = sourceCode;
@@ -62,21 +66,21 @@ namespace Fluxions {
 		return didCompile;
 	}
 
-	void RendererShader::create_shader(GLenum shaderType) {
+	void RendererShader::create_shader(GLenum shaderType_) {
 		if (shader)
 			delete_shader();
-		type = shaderType;
-		shader = glCreateShader(type);
+		shaderType = shaderType_;
 		source = "";
 		didCompile = false;
 		hadError = false;
+		shader = glCreateShader(shaderType);
 		HFLOGDEBUG("shader %d created.", shader);
 	}
 
 	void RendererShader::delete_shader() {
 		if (shader != 0) {
-			HFLOGDEBUG("shader %d deleted.", shader);
 			glDeleteShader(shader);
+			HFLOGDEBUG("shader %d deleted.", shader);
 			shader = 0;
 		}
 	}
