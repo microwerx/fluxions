6cc572373040bf9dc5adf1b044e8e7a77a47f26e
Jonathan Metzgar <microwerx@outlook.com>
2019-12-21 08:22:55 -0900

    add prototype for new image loader

---
+++ include/fluxions_image_loader.hpp
@@ -0,0 +1,11 @@
+#ifndef FLUXIONS_IMAGE_LOADER_HPP
+#define FLUXIONS_IMAGE_LOADER_HPP
+
+#include <fluxions_gte_image.hpp>
+
+namespace Fluxions
+{
+	bool LoadImage4f(const std::string& path, Image4f& image);
+}
+
+#endif
