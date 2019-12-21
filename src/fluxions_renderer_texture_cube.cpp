ebe53279c591d520fdf9b2303cb2759e99899348
Jonathan Metzgar <microwerx@outlook.com>
2019-12-20 16:09:00 -0900

    remove unused code

--- src/fluxions_renderer_texture_cube.cpp
+++ src/fluxions_renderer_texture_cube.cpp
@@ -6,13 +6,7 @@ namespace Fluxions
 	RendererTextureCube::RendererTextureCube()
 		: RendererGpuTexture(GL_TEXTURE_CUBE_MAP) {}
 
-	//void RendererTextureCube::init(const std::string& name) {
-	//	RendererObject::init(name);
-	//	texture.init(name);
-	//}
-
-	//void RendererTextureCube::kill() {
-	//	texture.kill();
-	//	RendererObject::kill();
-	//}
+	const char* RendererTextureCube::type() const noexcept {
+		return "RendererTextureCube";
+	}
 }
