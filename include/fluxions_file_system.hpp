19ed3116f53323312777cc1a4bdc43041e6a2551
Jonathan Metzgar <microwerx@outlook.com>
2020-01-02 13:17:11 -0900

    add C++ filesystem header

--- include/fluxions_file_system.hpp
+++ include/fluxions_file_system.hpp
@@ -1,6 +1,7 @@
 #ifndef FLUXIONS_FILE_SYSTEM_HPP
 #define FLUXIONS_FILE_SYSTEM_HPP
 
+#include <filesystem>
 #include <fluxions_file_path_info.hpp>
 #include <fluxions_file_reader_writer.hpp>
 #include <fluxions_fileio_iostream.hpp>
