50393da2fc6c7928c228ef4612bff73378089311
microwerx <jmetzgar@outlook.com>
2020-10-25 20:16:56 -0600

    Move viperfish code to viperfish project

--- src/viperfish_pch.cpp
+++
@@ -1,1 +0,0 @@
-#include "viperfish_pch.hpp"
--- src/viperfish_pch.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <viperfish_base.hpp>
-#include <viperfish.hpp>
-
-#ifdef MAX_RUN_MESSAGES
-#undef MAX_RUN_MESSAGES
-#endif
-#define MAX_RUN_MESSAGES 0
